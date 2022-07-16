e4e14bde52b8227cd12329a9cd376d871d8d4c1e
Jean809 <jean_carlosrosario709@outlook.com>
2022-07-16 09:31:38 -0400

    hidden class has been added to button close

--- script.js
+++ script.js
@@ -18,5 +18,11 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", function () {
     console.log("button clicked");
     modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
   });
 }
+
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
