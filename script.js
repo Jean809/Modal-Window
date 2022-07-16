1c33aaa5fbb7ad50c9a56c03a3ca8f9e629c426f
Jean809 <jean_carlosrosario709@outlook.com>
2022-07-16 12:17:20 -0400

    Refactoring code for 3rd time

--- script.js
+++ script.js
@@ -7,13 +7,6 @@ const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 console.log(btnsOpenModal);
 
-/*
-addEventListener("click", function () {
-  console.log(btnsOpenModal[0]);
-  console.log(btnsOpenModal[1]);
-  console.log(btnsOpenModal[2]);
-}); */
-
 function openModal() {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
@@ -24,8 +17,15 @@ function closeModal() {
   overlay.classList.add("hidden");
 }
 
+function closeModalWithOverlayClick() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", openModal);
 }
 
 btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
