803a3521a8348a1f06c1a5cdf1f54f8360f6a7a3
Jean809 <jean_carlosrosario709@outlook.com>
2022-07-17 08:25:46 -0400

    Escape keyword was added to hide modal & over.

--- script.js
+++ script.js
@@ -29,3 +29,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener("click", closeModal);
 
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+});
